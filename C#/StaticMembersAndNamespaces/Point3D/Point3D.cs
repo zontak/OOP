77202fd6c114623e51497db2373ad7c8e3ddf5f5
Kristiyan <zontak@mail.bg>
2014-09-23 15:32:23 +0300

    OOP

---
+++ C#/StaticMembersAndNamespaces/Point3D/Point3D.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point3D
+{
+    class Point3D
+    {
+        private double X { get; set; }
+        private double Y { get; set; }
+        private double Z { get; set; }
+
+        private static readonly Point3D StartingPoing = new Point3D(0, 0, 0);
+
+        public Point3D(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder str = new StringBuilder();
+            str.Append("X: " + this.X + ", Y: " + this.Y + ", Z: " + this.Z);
+            return str.ToString();
+        }
+
+        public static double CalcDistances(Point3D p1, Point3D p2)
+        {
+            double part1 = p2.X - p1.X;
+            double part2 = p2.Y - p1.Y;
+            double part3 = p2.Z - p1.Z;
+            double result = part1 * part1 + part2 * part2 + part3 * part3;
+            result = Math.Sqrt(result);
+            return result;
+        }
+    }
+}
---
+++ C#/StaticMembersAndNamespaces/Point3D/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Point3D
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point3D p1 = new Point3D(1, 2, 3);
+            Point3D p2 = new Point3D(5, 6, 7);
+
+            Console.WriteLine(Point3D.CalcDistances(p1, p2));
+        }
+    }
+}
